8394c15feb2b7631d9b165e1571a167f571208dc
gareth <danger@alphashack.com>
2013-03-22 15:01:27 +1100

    stampy will orientate into 8 different directions

--- ox/Ox.m
+++ ox/Ox.m
@@ -70,23 +70,43 @@
     int orientation = 2;
     
     // left
-    if (angle >= 135 && angle < 225) {
+    if (angle >= 157 && angle < 202) {
         orientation = 1;
     }
     
     // right
-    if (angle <= 45 || angle > 315 ) {
+    if (angle <= 22 || angle > 337 ) {
         orientation = 2;
     }
     
     // up
-    if (angle > 45 && angle <= 135) {
+    if (angle > 67 && angle <= 112) {
         orientation = 3;
     }
     
     // down
-    if (angle >= 225 && angle <= 315) {
+    if (angle >= 247 && angle <= 292) {
         orientation = 4;
+    }
+    
+    // up_left
+    if (angle >= 112 && angle <= 157) {
+        orientation = 5;
+    }
+    
+    // up_right
+    if (angle >= 22 && angle <= 67) {
+        orientation = 6;
+    }
+    
+    // down_left
+    if (angle >= 202 && angle <= 247) {
+        orientation = 7;
+    }
+    
+    // down_right
+    if (angle >= 292 && angle <= 337) {
+        orientation = 8;
     }
     
     return orientation;
@@ -95,15 +115,17 @@
 /*
     This function switches the sprite depending on which orientation
     is provided. Supported values are:
-        * left      : 1
-        * right     : 2
-        * up        : 3
-        * down      : 4
+        * left          : 1
+        * right         : 2
+        * up            : 3
+        * down          : 4
+        * up_left       : 5
+        * up_right      : 6
+        * down_left     : 7
+        * down_right    : 8
  */
 - (void) orientate:(int)withOrientation
 {
-    NSLog(@"orientate: %i", withOrientation);
-    
     if (_spriteBody != nil) {
         [self removeChild:_spriteBody cleanup:YES];
     }
@@ -114,35 +136,50 @@
     
     switch(withOrientation)
     {
-        case 1:
-            _spriteBody = [_loader spriteWithUniqueName:@"body_basic_pos_2" atPosition:ccp(0,0) inLayer:self];
-            _spriteHead = [_loader spriteWithUniqueName:@"head_position_2-5_reg_orange" atPosition:ccp(-37, 0) inLayer:self];
-    
-            [self setContentSize:_spriteBody.contentSize];
+        case 1: // left
+            _spriteBody = [_loader spriteWithUniqueName:@"body_basic_pos_4" atPosition:ccp(0,0) inLayer:self];
+            _spriteHead = [_loader spriteWithUniqueName:@"head_position_2-5_reg_orange" atPosition:ccp(-43, 0) inLayer:self];
             break;
-        case 2:
+        case 2: // right
+            // diaginal = ccp(17, 2)
             _spriteBody = [_loader spriteWithUniqueName:@"body_basic_pos_5" atPosition:ccp(0,0) inLayer:self];
-            _spriteHead = [_loader spriteWithUniqueName:@"head_position_2-5_reg_orange" atPosition:ccp(17, 2) inLayer:self];
-            
-            [self setContentSize:_spriteBody.contentSize];
+            _spriteHead = [_loader spriteWithUniqueName:@"head_position_2-5_reg_orange" atPosition:ccp(21, 0) inLayer:self];
             break;
-        case 3:
+        case 3: // up
             _spriteBody = [_loader spriteWithUniqueName:@"body_basic_pos_8" atPosition:ccp(0,0) inLayer:self];
             _spriteHead = [_loader spriteWithUniqueName:@"head_position_6-8_reg_orange" atPosition:ccp(-6, 30) inLayer:self];
             
             [self reorderChild:_spriteBody z:20];
             [self reorderChild:_spriteHead z:10];
-            
-            [self setContentSize:_spriteBody.contentSize];
             break;
             
-        case 4:
+        case 4: // down
             _spriteBody = [_loader spriteWithUniqueName:@"body_basic_pos_1" atPosition:ccp(0,0) inLayer:self];
             _spriteHead = [_loader spriteWithUniqueName:@"head_position_2-5_reg_orange" atPosition:ccp(-7, -20) inLayer:self];
+            break;
+        
+        case 5: //up_left
+            _spriteBody = [_loader spriteWithUniqueName:@"body_basic_pos_6" atPosition:ccp(0,0) inLayer:self];
+            _spriteHead = [_loader spriteWithUniqueName:@"head_position_2-5_reg_orange" atPosition:ccp(-37, 5) inLayer:self];
+            break;
             
-            [self setContentSize:_spriteBody.contentSize];
+        case 6: // up_right
+            _spriteBody = [_loader spriteWithUniqueName:@"body_basic_pos_7" atPosition:ccp(0,0) inLayer:self];
+            _spriteHead = [_loader spriteWithUniqueName:@"head_position_2-5_reg_orange" atPosition:ccp(17, 2) inLayer:self];
+            break;
+            
+        case 7: // down_left
+            _spriteBody = [_loader spriteWithUniqueName:@"body_basic_pos_2" atPosition:ccp(0,0) inLayer:self];
+            _spriteHead = [_loader spriteWithUniqueName:@"head_position_2-5_reg_orange" atPosition:ccp(-43, -5) inLayer:self];
+            break;
+            
+        case 8: // down_right
+            _spriteBody = [_loader spriteWithUniqueName:@"body_basic_pos_3" atPosition:ccp(0,0) inLayer:self];
+            _spriteHead = [_loader spriteWithUniqueName:@"head_position_2-5_reg_orange" atPosition:ccp(17, -2) inLayer:self];
             break;
     }
+    
+    [self setContentSize:_spriteBody.contentSize];
 }
 
 - (void) dealloc
